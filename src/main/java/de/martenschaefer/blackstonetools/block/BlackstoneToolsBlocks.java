0a59864abb96f2ff02347d5577448347464f79cf
mschae23 <s.marten72@icloud.com>
2020-06-13 15:09:57 +0200

    Updated to 1.16-pre5

--- src/main/java/de/martenschaefer/blackstonetools/block/BlackstoneToolsBlocks.java
+++ src/main/java/de/martenschaefer/blackstonetools/block/BlackstoneToolsBlocks.java
@@ -3,16 +3,15 @@ package de.martenschaefer.blackstonetools.block;
 import java.util.function.ToIntFunction;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.FurnaceBlock;
 import net.minecraft.block.Material;
 import net.minecraft.state.property.Properties;
 
 public class BlackstoneToolsBlocks {
  
-	public static final BlackstoneFurnaceBlock BLACKSTONE_FURNACE = new BlackstoneFurnaceBlock(AbstractBlock.Settings.of(Material.STONE).strength(3.8f, 3.8f).lightLevel(createLightLevelFromBlockState(13)));
+	public static final FurnaceBlock BLACKSTONE_FURNACE = new BlackstoneFurnaceBlock(AbstractBlock.Settings.of(Material.STONE).requiresTool().strength(3.8f, 3.8f).lightLevel(createLightLevelFromBlockState(13)));
 
 	private static ToIntFunction<BlockState> createLightLevelFromBlockState(int litLevel) {
-  return (blockState) -> {
-     return (Boolean)blockState.get(Properties.LIT) ? litLevel : 0;
-  };
+  return (blockState) -> blockState.get(Properties.LIT)? litLevel : 0;
 }
 }
