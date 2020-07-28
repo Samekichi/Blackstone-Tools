67ab19ce5636529802d2003a72865a40310d6cce
mschae23 <s.marten72@icloud.com>
2020-07-28 11:35:37 +0200

    Fixed crash when Vanilla Excavators and SuperAxes are not installed

--- src/main/java/de/martenschaefer/blackstonetools/item/BlackstoneToolsItems.java
+++ src/main/java/de/martenschaefer/blackstonetools/item/BlackstoneToolsItems.java
@@ -5,6 +5,7 @@ import com.github.levoment.superaxes.SuperAxesMaterialGenerator;
 import com.github.levoment.superaxes.SuperAxesMod;
 import draylar.magna.item.ExcavatorItem;
 import draylar.ve.VanillaExcavators;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 
@@ -15,6 +16,8 @@ public class BlackstoneToolsItems {
 	public static final Item BLACKSTONE_AXE = new BlackstoneAxeItem(new Item.Settings().group(ItemGroup.TOOLS));
 	public static final Item BLACKSTONE_SHOVEL = new BlackstoneShovelItem(new Item.Settings().group(ItemGroup.TOOLS));
 	public static final Item BLACKSTONE_HOE = new BlackstoneHoeItem(new Item.Settings().group(ItemGroup.TOOLS));
-	public static final ExcavatorItem BLACKSTONE_EXCAVATOR = new ExcavatorItem(BlackstoneToolMaterials.EXCAVATOR, 4, -2.6f, new Item.Settings().group(VanillaExcavators.GROUP));
-	public static final SuperAxeItem BLACKSTONE_SUPERAXE = new SuperAxeItem(new SuperAxesMaterialGenerator(BlackstoneToolMaterials.TOOLS), new Item.Settings().group(SuperAxesMod.SUPERAXES_GROUP));
+	public static final ExcavatorItem BLACKSTONE_EXCAVATOR = FabricLoader.getInstance().isModLoaded("vanillaexcavators")?
+									new ExcavatorItem(BlackstoneToolMaterials.EXCAVATOR, 4, -2.6f, new Item.Settings().group(VanillaExcavators.GROUP)) : null;
+	public static final SuperAxeItem BLACKSTONE_SUPERAXE = FabricLoader.getInstance().isModLoaded("lvmnt")?
+									new SuperAxeItem(new SuperAxesMaterialGenerator(BlackstoneToolMaterials.TOOLS), new Item.Settings().group(SuperAxesMod.SUPERAXES_GROUP)) : null;
 }
